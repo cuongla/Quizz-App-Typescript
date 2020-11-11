3ce176eae054aa6fce90c634bd5d1f88edb9020e
Tin La <tinla14@gmail.com>
2020-11-11 11:17:47 -0800

    Update api and add type/enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import {fetchQuizQuestions, Difficulty} from './API';
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers,setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrvia = async () => {
 
   }
@@ -10,12 +23,28 @@ const App = () => {
   }
 
   const nextQuestion = () => {
-    
+
   }
 
   return (
     <div className="App">
-      Quiz
+      <h1>Typescript Quiz</h1>
+      <button
+        className="start"
+        onClick={startTrvia}
+      >Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Qurstion...</p>
+      {/* <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer = {userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
-const QuestionCard = () => (
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNumber: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ answers, question, callback, userAnswer, questionNumber, totalQuestions }) => (
     <div>
-        Question Card
+        <p className="number">
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer}} />
+                    </button>
+                </div>
+            ))}
+        </div>
     </div>
 )
 
